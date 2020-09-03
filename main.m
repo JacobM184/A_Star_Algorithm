cf20a39dcdedd4ba6919b18dca415d8e4ae3252d
JacobM184 <55066056+JacobM184@users.noreply.github.com>
2020-09-03 17:45:20 +1200

    Update main.m

--- main.m
+++ main.m
@@ -1,15 +1,18 @@
 % map coordinates
-% map_8.txt -> (14,1) & (1, 18)
+% map_8.txt -> (14,1), (4, 2), (2, 2), (14, 10) & (1, 18)
 % map_1.txt -> (2, 2), (6, 2), (10, 2), (2, 4), (10,14) & (12, 6)
 
-startCol = 2;
-startRow = 2;
-goalCol = 14;
-goalRow = 10;
+startCol = 1;
+startRow = 14;
+goalCol = 18;
+goalRow = 1;
 
-map = map_convert("map_1.txt");
+map = map_convert("map_8.txt");
 
 f = @() a_star(startCol, startRow, goalCol, goalRow, map);
 t = timeit(f);
 disp("Time: ");
 disp(t);
+
+path = a_star(startCol, startRow, goalCol, goalRow, map);
+plotmap(map, path)
